ea0e959ffc82364781cb1a195b828808f6710942
Ambrozie <ambroziepaval@gmail.com>
2018-01-01 20:29:33 +0200

    Computer Vision labs preparations.

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import org.opencv.core.Core;
+
+/**
+ * TODO Description!
+ * <p>
+ * Creator: Ambrozie
+ * Info: Main.class
+ * Date: 01/01/2018 20:01
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("OMR PRESENTATION Paval Ambrozie");
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    }
+}
